fa106379b316f78a88ed7eed995d56571a45c3ce
Bernard O'Connor <bernard.oconnor47@mail.dcu.ie>
2016-11-29 00:24:34 +0000

    finish reverseString question

--- turing.hs
+++ turing.hs
@@ -145,6 +145,70 @@ palindrome(s,c) = case (s,c) of
                     (5,'a') -> (5,'a',N) --Found A, reject
                     (5,'Y') -> (5,'Y',Y) --Accept the case for a single B input
                     (5,'X') -> (5,'X',N) --Something weird has happened, reject (double check if this is needed)
-                    
                     _      -> error "No Valid Transition"
 
+
+reverseString (s,c) = case (s,c) of
+                  -- F will mark the current first letter to be swapped
+                  -- X will mark that at the end it should be replaced by A
+                  -- Y will mark that at the end it should be replaced by B
+                  (0,' ') -> (0,' ',Y) --Accept
+                  (0,'X') -> (7,'a',R) --Convert markers back to letters until at end of string
+                  (0,'Y') -> (7,'b',R) --Convert markers back to letters until at end of string
+                  (0,'a') -> (8,'F',R) --State 1 deals with swapping an A, state 8 checks if F is the last letter remaining iff F == A
+                  (0,'b') -> (9,'F',R) --State 2 deals with swapping a B,  state 9 checks if F is the last letter remaining iff F == B
+
+                  --Find last letter to swap with A
+                  --Search for end of remaining input after finding an A
+                  (1,'a') -> (1,'a',R)
+                  (1,'b') -> (1,'b',R)
+                  (1,' ') -> (3,' ',L) --Found end, move left 1
+                  (1,'X') -> (3,'X',L) --Found end, move left 1
+                  (1,'Y') -> (3,'Y',L) --Found end, move left 1
+
+                  --Swap letter with an A
+                  (3,'a') -> (4,'X',L) --F should next be replaced by an A
+                  (3,'b') -> (5,'X',L) --F should next be replaced by a B
+
+                  --Replace F with an A
+                  (4,'a') -> (4,'a',L) --Keep looking for F
+                  (4,'b') -> (4,'b',L) --Keep looking for F
+                  (4,'F') -> (0,'a',R) --Replacement performed, start next letter swapping
+
+                  --Replace F with a B
+                  (5,'a') -> (5,'a',L) --Keep looking for F
+                  (5,'b') -> (5,'b',L) --Keep looking for F
+                  (5,'F') -> (0,'b',R) --Replacement performed, start next letter swapping
+
+                  --Find last letter to swap with B
+                  --Search for end of remaining input after finding a B
+                  (2,'a') -> (2,'a',R)
+                  (2,'b') -> (2,'b',R)
+                  (2,' ') -> (6,' ',L) --Found end, move left 1
+                  (2,'X') -> (6,'X',L) --Found end, move left 1
+                  (2,'Y') -> (6,'Y',L) --Found end, move left 1
+
+                  --Swap letter with B
+                  (6,'a') -> (4,'Y',L) --F should next be replaced by an A
+                  (6,'b') -> (5,'Y',L) --F should next be replaced by a B
+
+                  --Convert remaining marks to their representing letters
+                  (7,' ') -> (7,' ',Y) --Accept
+                  (7,'X') -> (7,'a',R)
+                  (7,'Y') -> (7,'b',R)
+
+                  --Check if F was the last remaining letter iff F == A
+                  (8,'a') -> (1,'a',R) --Was not last letter, proceed as normal
+                  (8,'b') -> (1,'b',R) --Was not last letter, proceed as normal
+                  (8,' ') -> (4,' ',L) --F was last letter, move into replacing F with A state
+                  (8,'X') -> (4,'X',L) --F was last letter, move into replacing F with A state
+                  (8,'Y') -> (4,'Y',L) --F was last letter, move into replacing F with A state
+
+                  --Check if F was the last remaining letter iff F == B
+                  (9,'a') -> (2,'a',R) --Was not last letter, proceed as normal
+                  (9,'b') -> (2,'b',R) --Was not last letter, proceed as normal
+                  (9,' ') -> (5,' ',L) --F was last letter, move into replacing F with B state
+                  (9,'X') -> (5,'X',L) --F was last letter, move into replacing F with B state
+                  (9,'Y') -> (5,'Y',L) --F was last letter, move into replacing F with B state
+                  
+                  _       -> error "No Valid Transition"
